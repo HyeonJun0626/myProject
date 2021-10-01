0a6a00e1267e230a3b887b2a578136b68bc3eb3e
hyeonjun <roo113@naver.com>
2021-10-01 11:33:14 +0900

    no message

--- src/test/java/com/MyprojectApplicationTests.java
+++ src/test/java/com/MyprojectApplicationTests.java
@@ -3,11 +3,14 @@ package com;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class MyprojectApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
+
